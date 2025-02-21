a3223d583ecb3431886fdd46b5bf0fbaedf7ae95
Gleydson Miranda <gleydsonmiranda73@gmail.com>
2025-02-21 16:01:12 -0300

    Projeto base

---
+++ src/javascript/script.js
@@ -0,0 +1,8 @@
+// document.getElementById('open_btn').addEventListener('click', function () {
+//     document.getElementById('sidebar').classList.toggle('open-sidebar');
+// });
+
+document.getElementById("open_btn").addEventListener("click", function() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("open-sidebar");
+});
